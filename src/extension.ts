3bb6d9e1c1cf7dc4a28beff560a787a6279bec48
WiRight <ruslan.mahin1@gmail.com>
2018-08-09 23:01:34 +0300

    add messageBox

--- src/extension.ts
+++ src/extension.ts
@@ -5,20 +5,41 @@ import axios from 'axios';
 
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand('extension.browsertab', () => {
-    const panel = vscode.window.createWebviewPanel(
-      'browserTab',
-      'browserTab',
-      vscode.ViewColumn.Active,
-      {
-        enableScripts: true
+    vscode.window.showInputBox({
+      prompt: 'Enter a site url. Example: http://localhost',
+      value: 'http://',
+      ignoreFocusOut: true,
+      placeHolder: 'Site url',
+      validateInput: (value: string): string => {
+        return '';
       }
-    );
-    // const url = 'http://localhost/page2.html';
-    const url = 'http://localhost';
-    getWebContent(url).then((content) => {
-      panel.webview.html = content.data;
+    }).then(url => {
+      if (url) {
+        const panel = vscode.window.createWebviewPanel(
+          'browserTab',
+          'browserTab',
+          vscode.ViewColumn.Active, {
+            enableScripts: true
+          }
+        );
+        console.log('get from user', url);
+        // Get HTML content of site
+        getWebContent(url).then((content) => {
+          // Convert link to style
+          getStyles(url, content.data).then((styles) => {
+            // Show data
+            panel.webview.html = [
+              styles,
+              content.data
+            ].join(' ')
+          });
+        }).catch((e) => {
+          console.error('getWebContent error', e)
+        });
+      } else {
+        vscode.window.showInformationMessage('The url can be specified! Do nothing');
+      }
     });
-    getBla(url);
   });
 
   context.subscriptions.push(disposable);
@@ -27,38 +48,52 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {
 }
 
-function getBla(url : string) {
-  getWebContent(url).then((content) => {
-    // Site content html
-    const data = content.data,
-      // Regexp for find all html tag <link>
-      findLinkRegexp = /\<link\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1/g,
+/**
+ * Convert <link> to <style>
+ * @param {string} url Base url
+ * @param {string} content HTML content by url
+ */
+function getStyles(url: string, content: string): Promise<string> {
+  return new Promise<string>((resolve) => {
+    // Regexp for find all html tag <link>
+    const findLinkRegexp = /\<link\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1/g,
       // Regexp for find attribute href in <link>
       findLinkHrefRegexp = /href=\"(.*?)\"/,
       // All founded <link>
-      links = data.match(findLinkRegexp);
+      links = content.match(findLinkRegexp);
     if (links && Array.isArray(links)) {
       // Css inline styles
       const cssStyles: string[] = [],
         // All axios promises for getting raw css
         cssPromises: Promise<any>[] = [];
       links.forEach((item) => {
+        // Try find href attribute
         const needle = item.match(findLinkHrefRegexp);
+        // If found
         if (needle && Array.isArray(needle) && needle.hasOwnProperty('1')) {
+          // Build url for getting css
           const cssUrl = url + '/' + needle[1];
           cssPromises.push(axios.get(cssUrl));
         }
       });
       if (cssPromises) {
+        // Getting all css's
         Promise.all(cssPromises).then((values) => {
           values.forEach((item) => {
-            cssStyles.push(item.data);
+            // Genetare inline style
+            cssStyles.push([
+              '<style type="text/css">',
+              item.data,
+              '</style>'
+            ].join(' '));
           });
-          console.log('cssStyles', cssStyles);
+          resolve(cssStyles.join(' '))
         });
       }
+    } else {
+      resolve('')
     }
-  }).catch((e) => console.error(e));
+  });
 }
 
 /**
