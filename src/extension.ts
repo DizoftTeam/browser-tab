4fe8911ec8c1376dc474524c7e9dfe32930d2aed
WiRight <ruslan.mahin1@gmail.com>
2018-08-09 16:26:22 +0300

    save work

--- src/extension.ts
+++ src/extension.ts
@@ -29,17 +29,34 @@ export function deactivate() {
 
 function getBla(url : string) {
   getWebContent(url).then((content) => {
+    // Site content html
     const data = content.data,
+      // Regexp for find all html tag <link>
       findLinkRegexp = /\<link\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1/g,
+      // Regexp for find attribute href in <link>
       findLinkHrefRegexp = /href=\"(.*?)\"/,
-      result = data.match(findLinkRegexp);
-    if (result && Array.isArray(result)) {
-      result.forEach((item) => {
+      // All founded <link>
+      links = data.match(findLinkRegexp);
+    if (links && Array.isArray(links)) {
+      // Css inline styles
+      const cssStyles: string[] = [],
+        // All axios promises for getting raw css
+        cssPromises: Promise<any>[] = [];
+      links.forEach((item) => {
         const needle = item.match(findLinkHrefRegexp);
         if (needle && Array.isArray(needle) && needle.hasOwnProperty('1')) {
-          console.log('need load by', url + '/' + needle[1]);
+          const cssUrl = url + '/' + needle[1];
+          cssPromises.push(axios.get(cssUrl));
         }
       });
+      if (cssPromises) {
+        Promise.all(cssPromises).then((values) => {
+          values.forEach((item) => {
+            cssStyles.push(item.data);
+          });
+          console.log('cssStyles', cssStyles);
+        });
+      }
     }
   }).catch((e) => console.error(e));
 }
