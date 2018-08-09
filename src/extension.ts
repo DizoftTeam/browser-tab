18ab6c083fb28926e14d3d07033aaea4623b36d8
WiRight <ruslan.mahin1@gmail.com>
2018-08-09 13:24:50 +0300

    initial commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,52 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
+import axios from 'axios';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "browser-tab" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+  const disposable = vscode.commands.registerCommand('extension.browsertab', () => {
+    const panel = vscode.window.createWebviewPanel(
+      'browserTab',
+      'browserTab',
+      vscode.ViewColumn.Active,
+      {
+        enableScripts: true
+      }
+    );
+    // const url = 'http://localhost/page2.html';
+    const url = 'http://localhost';
+    getWebContent(url).then((content) => {
+      panel.webview.html = content.data;
     });
+    getBla(url);
+  });
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+function getBla(url : string) {
+  getWebContent(url).then((content) => {
+    const data = content.data,
+      findLinkRegexp = /\<link\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1/g,
+      findLinkHrefRegexp = /href=\"(.*?)\"/,
+      result = data.match(findLinkRegexp);
+    if (result && Array.isArray(result)) {
+      result.forEach((item) => {
+        const needle = item.match(findLinkHrefRegexp);
+        if (needle && Array.isArray(needle) && needle.hasOwnProperty('1')) {
+          console.log('need load by', url + '/' + needle[1]);
+        }
+      });
+    }
+  }).catch((e) => console.error(e));
+}
+
+/**
+ * Get web content by URL
+ */
+function getWebContent(url: string): Promise<any> {
+  return axios.get(url);
 }
